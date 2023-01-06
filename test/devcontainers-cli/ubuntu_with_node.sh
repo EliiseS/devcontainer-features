fad0a56e1178cd9ab064a9ab762a0c48e9ccb2c8
Eliise <elseling@microsoft.com>
2023-01-06 16:51:43 +0000

    Fix test

--- test/devcontainers-cli/ubuntu_with_node.sh
+++ test/devcontainers-cli/ubuntu_with_node.sh
@@ -4,10 +4,23 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
+check_node() {
+    VERSION=$(node -v)
+    echo "$VERSION"
+    if [[ $VERSION == v16.* ]] # * is used for pattern matching
+    then
+        echo "Version is 16."
+        return 0
+    else
+        echo "Version is NOT 16!"
+        return 1
+    fi
+}
+
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 check "Check devcontainer CLI is installed" devcontainer --version
-check "Check node version is expected" [[ $(node -v) == v16.* ]] # * is used for pattern matching
+check "Check node version is expected" check_node 
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
